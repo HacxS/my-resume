d936da42be0b8088d87b249f53cce8609aeea332
HacxS <albertgovind17@gmail.com>
2022-08-06 18:21:33 +0530

    update

--- apps/resume/_content/Header/index.ts
+++ apps/resume/_content/Header/index.ts
@@ -3,6 +3,7 @@ import jsonData from './data.json';
 export interface IHeader {
   heading: string;
   description: string;
+  ogDescription: string;
 }
 
 export const data: IHeader = jsonData;
--- apps/resume/src/pages/_document.page.tsx
+++ apps/resume/src/pages/_document.page.tsx
@@ -6,7 +6,7 @@ import { authorImage, authorName, favicon, resumeGAID, resumeHotjarID } from 'co
 import { resumeUrl } from '../../_config';
 import { data } from '../../_content/Header';
 
-const TITLE = `${authorName} resume`;
+const TITLE = `${authorName}`;
 
 function isValid(value: any) {
   if (typeof value === 'string' && value.length > 0) return true;
@@ -24,7 +24,7 @@ export default class ResumeDocument extends Document {
           <link rel="icon" href={favicon.default.src} />
           <meta httpEquiv="Content-type" content="text/html; charset=utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta name="description" content={data.description} />
+          <meta name="description" content={data.ogDescription} />
           <link rel="canonical" href={resumeUrl} />
           <meta name="keywords" content="resume,development,developer" />
           {/* for korean keywords */}
@@ -33,11 +33,11 @@ export default class ResumeDocument extends Document {
           <meta property="og:locale" content="ko_KR" />
           <meta property="og:title" content={TITLE} />
           <meta property="og:url" content={resumeUrl} />
-          <meta property="og:description" content={data.description} />
+          <meta property="og:description" content={data.ogDescription} />
           <meta property="og:image" content={authorImage.default.src} />
           <meta name="twitter:creator" content={authorName} />
           <meta name="twitter:title" content={TITLE} />
-          <meta name="twitter:description" content={data.description} />
+          <meta name="twitter:description" content={data.ogDescription} />
           <meta name="twitter:image" content={authorImage.default.src} />
 
           {/* for google analytics */}
